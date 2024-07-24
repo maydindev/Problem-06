6f964a877df3caa4d2da1536254f0dca6c01bd75
maydindev <maydin.dev@outlook.com>
2024-07-24 14:15:27 +0300

    Init

---
+++ src/App.js
@@ -0,0 +1,10 @@
+// Her bir butonun üzerine, içerikte yazan eyleme karşılık gelen satır içi/inline stiller ekleyin
+export default function ButtonVariants() {
+  return (
+    <span className='wrapper'>
+      <button className='button-create'>Create</button>
+      <button className='button-update'>Update</button>
+      <button className='button-delete'>Delete</button>
+    </span>
+  )
+}
